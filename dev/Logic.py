57f26691c3cd3e34a5fcd9d43ae5b60728b5219c
boyasha <alexeybo17@yandex.ru>
2022-04-10 22:40:36 +0300

    version0.2

---
+++ dev/Logic.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Logic():
+    white = (255, 255, 255)
+    yellow = (255, 255, 102)
+    black = (0, 0, 0)
+    red = (213, 50, 80)
+    green = (0, 255, 0)
+    blue = (50, 153, 213)
+    
