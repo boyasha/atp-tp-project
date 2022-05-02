62f1076d7a15573a67f73761be2c9d4ef5361faa
boyasha <alexeybo17@yandex.ru>
2022-05-02 22:31:55 +0300

    checkpoint_3

---
+++ checkpoint_3/Food.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+
+
+class Food:
+    def __init__(self):
+        pass
+
+    def spawn_food(self, display, color, Globals):
+        pygame.draw.circle(display, color, [Globals.x_food, Globals.y_food], Globals.food_block/2)
+
+    def check_eat_food(self, Globals):
+        if abs(Globals.x_food-Globals.x_start) < Globals.food_block*1.3 and abs(Globals.y_food-Globals.y_start) < Globals.food_block*1.3:
+            Globals.length_snake += 1
+            Globals.snake_speed += 0.5
+            Globals.x_food = round(random.randrange(0, Globals.display_width - 4*Globals.food_block) / Globals.food_block) * Globals.food_block
+            Globals.y_food = round(random.randrange(0, Globals.display_height - 4*Globals.food_block) / Globals.food_block) * Globals.food_block
+
